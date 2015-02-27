2d4c8f8447a2c725e6423d87cfc144260a8cfdc8
Chris Waters <chris@first65.com>
2015-02-27 15:53:25 +0000

    cart test

--- src/Marmalade/Cart.php
+++ src/Marmalade/Cart.php
@@ -2,15 +2,15 @@
 namespace Marmalade;
 
 class Cart {
-  
+
   public $items;
   public $cart_name;
-  
+
   function __construct($name = 'cart') {
     $this->cart_name = $name;
     $this->retrieve_or_create_session();
   }
-  
+
   public function add_item($product_id, $quantity = 1) {
     if(array_key_exists($product_id, $this->items)){
       $this->increase_item_quantity($product_id, $quantity);
@@ -19,7 +19,7 @@ class Cart {
       $this->sync_session();
     }
   }
-  
+
   public function remove_item($product_id, $quantity = 1) {
     if(array_key_exists($product_id, $this->items)){
       if($this->quantity_of($product_id) > $quantity){
@@ -30,39 +30,43 @@ class Cart {
       }
     }
   }
-  
+
   public function quantity_of($product_id) {
     return $this->items[$product_id];
   }
-  
+
   public function get_item_ids() {
     return array_keys($this->items);
   }
-  
+
   public function total_count() {
     return array_sum(array_map(function($item) { return $item->quantity; }, $this->items()));
   }
-  
+
   public function total_price() {
     return array_sum(array_map(function($item) { return $item->total_price(); }, $this->items()));
   }
-  
+
+  public function total_cents() {
+    return intval($this->total_price() * 100);
+  }
+
   public function items() {
     $item_array = $this->items;
     array_walk($item_array, function(&$quantity, $product_id) { $quantity = new CartItem($product_id, $quantity); });
     return array_filter($item_array, function($item) { return $item->is_valid(); });
   }
-  
+
   private function increase_item_quantity($product_id, $quantity) {
     $_SESSION[$this->cart_name][$product_id] += $quantity;
     $this->sync_session();
   }
-  
+
   private function decrease_item_quantity($product_id, $quantity) {
     $_SESSION[$this->cart_name][$product_id] -= $quantity;
     $this->sync_session();
   }
-   
+
   private function retrieve_or_create_session() {
     if(isset($_SESSION[$this->cart_name])){
       $this->sync_session();
@@ -71,7 +75,7 @@ class Cart {
       $this->sync_session();
     }
   }
-  
+
   private function sync_session() {
     $this->items = $_SESSION[$this->cart_name];
   }
--- tests/CartTest.php
+++ tests/CartTest.php
@@ -10,58 +10,66 @@ class CartTest extends PHPUnit_Framework_TestCase
     $this->assertEquals('foo', $cart->cart_name);
     $this->assertArrayHasKey('foo', $_SESSION);
   }
-  
+
   public function test_add_item(){
     $cart = new \Marmalade\Cart('foo');
-    
+
     //Test new product adds 1 item
     $cart->add_item(12);
     $this->assertEquals(1, $cart->items[12]);
-    
+
     //Test quantity parameter and increments quantity
     $cart->add_item(12, 2);
     $this->assertEquals(3, $cart->items[12]);
-    
+
     //Test handling multiple products
     $cart->add_item(14, 2);
     $this->assertEquals(3, $cart->items[12]);
     $this->assertEquals(2, $cart->items[14]);
   }
-  
+
   public function test_remove_item(){
     $cart = new \Marmalade\Cart('foo');
     $cart->add_item(12, 2);
-    
+
     //Reduce it
     $cart->remove_item(12);
     $this->assertEquals(1, $cart->items[12]);
-    
+
     //Remove it completely
     $cart->remove_item(12);
     $this->assertArrayNotHasKey(12, $cart->items);
   }
-  
+
   public function test_quantity_of(){
     $cart = new \Marmalade\Cart('foo');
     $cart->add_item(12, 2);
     $this->assertEquals(2, $cart->quantity_of(12));
   }
-  
+
   public function test_total_price(){
     $cart = new \Marmalade\Cart('foo');
     $cart->add_item(12, 2);
     $cart->add_item(14, 4);
     $this->assertEquals(600, $cart->total_price());
-    
+
     $cart->remove_item(12);
     $this->assertEquals(500, $cart->total_price());
   }
-  
+
+  public function test_total_cents(){
+    $cart = new \Marmalade\Cart('foo');
+    $cart->add_item(12, 2);
+    $cart->add_item(14, 4);
+    $this->assertEquals(600, $cart->total_price());
+    $this->assertEquals(60000, $cart->total_cents());
+  }
+
   public function test_total_count(){
     $cart = new \Marmalade\Cart('foo');
     $cart->add_item(12, 55);
     $cart->add_item(14, 45);
     $this->assertEquals(100, $cart->total_count());
   }
-  
+
 }
