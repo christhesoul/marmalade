32bf8a8b8eb406d42b7a8e8e2d163ddd61ad190a
Chris Waters <chris@first65.com>
2015-03-02 16:59:48 +0000

    nothing

--- src/Marmalade/CartItem.php
+++ src/Marmalade/CartItem.php
@@ -2,13 +2,13 @@
 namespace Marmalade;
 
 class CartItem {
-  
+
   public $name;
   public $quantity;
   public $product_id;
-  
+
   private $valid;
-  
+
   function __construct($product_id, $quantity) {
     if(get_post($product_id)){
       $this->name = get_post($product_id)->post_title;
@@ -19,17 +19,17 @@ class CartItem {
       $this->valid = false;
     }
   }
-  
+
   public function is_valid() {
     return $this->valid;
   }
-  
+
   public function single_price() {
     return get_field('price', $this->product_id);
   }
-  
+
   public function total_price() {
     return $this->single_price() * $this->quantity;
   }
-  
+
 }
