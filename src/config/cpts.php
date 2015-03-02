ffa69f0c4db580ea961e3d074a37986e574c3319
Chris Waters <chris@first65.com>
2015-03-02 14:15:44 +0000

    add cpts

--- src/config/cpts.php
+++ src/config/cpts.php
@@ -3,7 +3,7 @@ function marmalade_cpts_register() {
 
 	$cpts = array(
     new Marmalade\CPT('products','Product','Products'),
-    new Marmalade\CPT('orders','Order','Orders')
+    new Marmalade\CPT('orders','Order','Orders', array('title'))
   );
 
   foreach($cpts as $cpt){
