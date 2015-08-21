b3d68bd198de1db6cc18df2849d5a0ad2f72d261
Chris Waters <chris@first65.com>
2015-08-21 13:40:09 +0100

    namespace ajax function

--- src/config/ajax.php
+++ src/config/ajax.php
@@ -1,7 +1,7 @@
 <?php
-add_action('wp_head','ajaxurl');
+add_action('wp_head','marmalade_ajaxurl');
 
-function ajaxurl() { ?>
+function marmalade_ajaxurl() { ?>
   <script type="text/javascript">
   var ajaxurl = '<?php echo admin_url('admin-ajax.php'); ?>';
   </script>
